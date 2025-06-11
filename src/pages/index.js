3933fcd763af8f2b38aa64b00884a15245ae09a9
JGamez17 <github email address>
2025-06-11 17:01:11 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home(){
+  return (
+    <div className="max-w-3xl mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">App Rating Advisor </h1>
+    </div>  
+  );
+};      
